22e3b3b13160e90807c582933bdad9d4614a2334
Ali <aliahmadi.xx90@gmail.comOmit>
2024-11-25 11:31:28 +0330

    I create new user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomeUser(AbstractUser):
+    CLASSNAME_CHOICES = (
+        ("7.1", "7.1"),
+        ("7.2", "7.2"),
+        ("7.3", "7.3"),
+        ("8.1", "8.1"),
+        ("8.2", "8.2"),
+        ("8.3", "8.3"),
+        ("9.1", "9.1"),
+        ("9.2", "9.2"),
+        ("9.3", "9.3"),
+    )
+    fullname = models.CharField('نام و نام خانوادگی', max_length=255)
+    classname = models.CharField(
+        'کلاس', max_length=20, choices=CLASSNAME_CHOICES)
+    joined_number = models.PositiveBigIntegerField('شماره عضویت', null=True)
+    
