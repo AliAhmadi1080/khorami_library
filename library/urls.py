22e609a86ad2a118dcb3e05d8d77b2e6e0aa0e09
aliahmadi <aliahmadi.xx90@gmail.com>
2024-12-05 18:31:15 +0330

    now we can see loans.

--- library/urls.py
+++ library/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import import_excle_file, dashbord, search, create_user, create_loan
+from .views import (import_excle_file, dashbord,undo_loan,
+                    search_user, search, create_user, create_loan)
 
 urlpatterns = [
     path("accounts/login/", views.LoginView.as_view(), name="login"),
     path('import_file/', import_excle_file, name='import_excle_file'),
     path('dashbord/', dashbord, name='dashbord'),
+    path('undo_return/<int:loan_id>', undo_loan, name='undo_return'),
+    path('search_user/', search_user, name='search_user'),
     path('create_loan/', create_loan, name='create_loan'),
     path('create_user/', create_user, name='create_user'),
     path('', search, name='search'),
