84abff8e08139992e1dba5d467d758cc41513515
Ali <aliahmadi.xx90@gmail.comOmit>
2025-01-01 14:16:37 +0330

    I create Post and Category models

--- library/models.py
+++ library/models.py
@@ -25,3 +25,15 @@ class Loan(models.Model):
     def __str__(self):
         return f"{self.user} کتاب {self.book} را در \
             تاریخ {self.loan_date} قرض گرفته است."
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category, related_name="posts")
