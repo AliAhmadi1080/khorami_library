7378ae71890719c6b4f531e4e8a48f1f61664d2c
Ali <aliahmadi.xx90@gmail.comOmit>
2025-08-17 12:02:21 +0330

    Refactor ScoreEntry model to use jDateField for date and CharField for reason

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import (AbstractBaseUser,
                                         BaseUserManager, PermissionsMixin)
 from django.contrib.auth.hashers import make_password
+from django_jalali.db import models as jmodels
 from django.db import models
 
 
@@ -55,11 +56,13 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.fullname
 
+
 class ScoreEntry(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='score_entries')
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='score_entries')
     score = models.IntegerField()
-    reason = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    reason = models.CharField(max_length=255)
+    date = jmodels.jDateField(auto_now_add=True)
 
     def str(self):
         return f"{self.user.username} | {self.score} | {self.reason} | {self.date.strftime('%Y-%m-%d')}"
